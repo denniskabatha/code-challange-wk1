fd4d8cdad171b7da4f46817463536a7853a0643c
denniskabatha <162292636+denniskabatha@users.noreply.github.com>
2024-03-25 10:44:08 +0300

    Update script.js

--- speed detector/script.js
+++ speed detector/script.js
@@ -7,19 +7,37 @@ function checkSpeed() {
         document.getElementById("demo").innerHTML = "Ok"
         
     }else if (number<=75) {
-        document.getElementById("demo1").innerHTML = "2 points"
+        document.getElementById("demo").innerHTML = "2 points"
     }else if (number<=80) {
-        document.getElementById("demo1").innerHTML = "4 points"
+        document.getElementById("demo").innerHTML = "4 points"
     }else if (number<=85) {
-        document.getElementById("demo1").innerHTML = "6 points"
+        document.getElementById("demo").innerHTML = "6 points"
     }else if (number<=90) {
-        document.getElementById("demo1").innerHTML = "8 points"
+        document.getElementById("demo").innerHTML = "8 points"
     }else if (number<=95) {
-        document.getElementById("demo1").innerHTML = "10 points"
+        document.getElementById("demo").innerHTML = "10 points"
     }else if (number<=100) {
-        document.getElementById("demo1").innerHTML = "12 points"
+        document.getElementById("demo").innerHTML = "12 points"
     }else if (number<=105) {
-        document.getElementById("demo1").innerHTML = "licence suspended"
+        document.getElementById("demo").innerHTML = "licence suspended"
+    }else if (number<=110) {
+        document.getElementById("demo").innerHTML = "licence suspended"
+    }else if (number<=115) {
+        document.getElementById("demo").innerHTML = "licence suspended"
+    }else if (number<=120) {
+        document.getElementById("demo").innerHTML = "licence suspended"
+    }else if (number<=125) {
+        document.getElementById("demo").innerHTML = "licence suspended"
+    }else if (number<=130) {
+        document.getElementById("demo").innerHTML = "licence suspended"
+    }else if (number<=135) {
+        document.getElementById("demo").innerHTML = "licence suspended"
+    }else if (number<=140) {
+        document.getElementById("demo").innerHTML = "licence suspended"
+    }else if (number<=145) {
+        document.getElementById("demo").innerHTML = "licence suspended"
+    }else if (number<=150) {
+        document.getElementById("demo").innerHTML = "licence suspended"
     }
 
 }
